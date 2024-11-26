86f063ba3b042b0d17fc16f309eb5bc7c1f0dfcd
prismgentem <foksfire12345px@gmail.com>
2024-11-27 00:42:30 +0300

    init

---
+++ src/main/java/org/example/fj_16_metrics_log/Fj16MetricsLogApplication.java
@@ -0,0 +1,13 @@
+package org.example.fj_16_metrics_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fj16MetricsLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fj16MetricsLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/fj_16_metrics_log/Fj16MetricsLogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fj_16_metrics_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fj16MetricsLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
